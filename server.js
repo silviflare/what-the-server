422c2063d8b4aff70364e4f5d2236af43d1dfa21
Silvia Flare <silviaguizan@gmail.com>
2023-05-17 18:53:30 +0200

    API_URL

--- server.js
+++ server.js
@@ -3,10 +3,6 @@ const app = require("./app");
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 5005
 const PORT = process.env.PORT || 5005;
 
-/* app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server listening on http://localhost:${PORT}`);
-}); */
-
-app.listen(3000, "0.0.0.0", () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server listening on http://localhost:${PORT}`);
 });
